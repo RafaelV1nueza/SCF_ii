1e5083e913225d31734867a3afde4800a680c860
RafaelV1nueza <a01706351@tec.mx>
2022-11-10 17:49:08 -0600

    Cam Update

--- src/cam_vec_tran.py
+++ src/cam_vec_tran.py
@@ -1,25 +1,48 @@
 import rospy
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import Pose
 from tf.transformations import quaternion_from_euler
+
 import numpy as np 
 
+class CamInfoClass():
+    # 
+    def __init__(self): 
+        rospy.on_shutdown(self.cleanup) 
+        
+        self.pub_state = rospy.Publisher('vectorPose_UR', Pose, queue_size=1)
+        rospy.Subscriber("visp_auto_tracker/object_position", Pose, self.cam_vector)
+
+        self.tag = Pose()
+
+        #********** INIT NODE **********### 
+        r = rospy.Rate(2) #1Hz 
+        print("Node initialized 2hz")
+
+        
+    def cam_vector(self,tag_vector):
+        self.tag = tag_vector.pose
+
+        print('Recieved Vector Pose')
 
 
-def cam_info(self, x_goal, y_goal, yaw):
-        # Creates a new goal with the MoveBaseGoal constructor
-        # it receives x_goal and y_goal [m] and yaw [rad]
-        # All the coordinates are measured with respect to the "/map" frame. 
-        # It returns a MoveBaseGoal pose.
-        goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = "/map"
-        goal.target_pose.header.stamp = rospy.Time.now()
-        # Position
-        goal.target_pose.pose.position.x = x_goal
-        goal.target_pose.pose.position.y = y_goal
-        # Rotation of the mobile base frame w.r.t. "map" frame as a quaternion
-        quat = quaternion_from_euler(0,0,yaw)
-        goal.target_pose.pose.orientation.x = quat[0]
-        goal.target_pose.pose.orientation.y = quat[1]
-        goal.target_pose.pose.orientation.z = quat[2]
-        goal.target_pose.pose.orientation.w = quat[3]
-        return goal
+
+    def cleanup(self): 
+        #This function is called just before finishing the node 
+        # You can use it to clean things up before leaving 
+        # Example: stop the robot before finishing a node.   
+        
+        #kill the robot
+        print("---------------xxxxx-------------------Ded")
+        print("                  __")
+        print("     w  c(..)o   (")
+        print("      \__(-)    __)")
+        print("          /\   (")
+        print("         /(_)___)")
+        print("         w /|")
+        print("          | |")
+        print("Vinu     m  m")    
+
+if __name__ == "__main__": 
+    print("Hihi")
+    rospy.init_node("tag_vecTf", anonymous=True) 
+    CamInfoClass()
