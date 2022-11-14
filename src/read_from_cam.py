9a1e47e5100591302ef7550e25c4f6b2c37c82bc
vmino28 <vmino28@hotmail.com>
2022-11-14 17:23:09 -0600

    commit

--- src/read_from_cam.py
+++ src/read_from_cam.py
@@ -23,7 +23,7 @@ def talker():
 
         # Our operations on the frame comes here
         #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+ 
         # Display the resulting frame
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
