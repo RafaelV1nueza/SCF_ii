52616de57936ea107c0a05ba9bcd085108611d7b
Yochiro14 <a01705059@tec.mx>
2022-11-14 12:33:35 -0600

    header

--- src/collabROB_brain.py
+++ src/collabROB_brain.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
 
-# -- BEGIN LICENSE BLOCK ----------------------------------------------
-# Copyright 2021 FZI Forschungszentrum Informatik
-# Created on behalf of Universal Robots A/S
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# -- END LICENSE BLOCK ------------------------------------------------
-#
-# ---------------------------------------------------------------------
-# !\file
-#
-# \author  Felix Exner mauch@fzi.de
-# \date    2021-08-05
-#
+#########################################################################
+#   _________  ___________ ___________ ____   ____ ___________          #
+#  /   _____/  \__    ___/ \_   _____/ \   \ /   / \_   _____/          #
+#  \_____  \     |    |     |    __)_   \   Y   /   |    __)_           #
+#  /        \    |    |     |        \   \     /    |        \          #
+# /_______  / /\ |____| /\ /_______  / /\ \___/ /\ /_______  / /\       #
+#         \/  \/        \/         \/  \/       \/         \/  \/       #
+#                                                                       #
+#########################################################################
+#                                                                       #
+# File Name: ur_node.py                                                 #
+#                                                                       #
+# Maintainer: vinu/josh                                                 #
+#                                                                       #
+# Version: v1.0.1 (alpha)                                               #
+#                                                                       #
+# Notes: Reads x y and z pose form a tf topic. To add: publish.         #
+#                                                                       #
+#                                                                       #
+#                                                                       #
+# Latest edit: vinu                                                     #
+#                                                                       #
+# Date: 14.11.2022                                                      #
+#########################################################################
 #
 # ---------------------------------------------------------------------
 import sys
